6553323ce981b5138a67a3159f0243c0a687e09b
tom <asdf@>
2024-12-15 17:23:53 +0200

    new project

--- Noam.py
+++ Noam.py
@@ -2,3 +2,6 @@ def print_funny_story():
     print("why did the chiken crossed the road?")
     print("because yomum wanted to")
 print_funny_story()
+
+def print_childhood_story():
+    print("first time riding on bicycle")
